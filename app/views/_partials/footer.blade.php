603c63dd4cc3939d9815bc5b554cc1cdccf20c9f
Benjamin Russell <ben.russell@domo.com>
2014-09-29 23:42:53 -0600

    tweaked top border of footer. also added dynamic copyright year

--- app/views/_partials/footer.blade.php
+++ app/views/_partials/footer.blade.php
@@ -1,3 +1,3 @@
 <section class="container footer">
-    <p class="text-center">All works &copy; Becca Bolduc - Provo, Utah</p>
+    <p class="text-center">&copy; {{ date('Y') }} Becca Bolduc - Provo, Utah</p>
 </section>

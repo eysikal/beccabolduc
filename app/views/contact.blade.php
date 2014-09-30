4dbe7d0d34b684d328541f5030e574b75490162a
Benjamin Russell <ben.russell@domo.com>
2014-09-30 10:17:06 -0600

    added social links to contact page

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
 
-<section class="container text-center">
-    
+<section class="container">
+    <ul class="list-unstyled text-center social">
+        <li><a href=""><img src="/assets/images/twitter.jpg"></a></li>
+        <li><a href="https://www.facebook.com/becca.bolduc.1"><img src="/assets/images/facebook.jpg"></a></li>
+        <li><a href=""><img src="/assets/images/instagram.jpg"></a></li>
+    </ul>
 </section>
 
 @stop

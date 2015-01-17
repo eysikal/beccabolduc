b84dbf3fbc8da2d54bbbd69dd88ee14bcda50212
eysikal <eysikal@gmail.com>
2015-01-17 00:28:23 +0000

    added some new arts and also modified the contact page

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -6,6 +6,7 @@
     <ul class="list-unstyled text-center social">
         <!-- <li><a href=""><img src="/assets/images/twitter.jpg"></a></li> -->
         <li><a href="https://www.facebook.com/beccabolducart"><img src="/assets/images/facebook.jpg"></a></li>
+        <li><a href=mailto:beccabolduc05@gmail.com>beccabolduc05@gmail.com</a></li>
         <!-- <li><a href=""><img src="/assets/images/instagram.jpg"></a></li> -->
     </ul>
 </section>
